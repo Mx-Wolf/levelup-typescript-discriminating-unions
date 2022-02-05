031fd1c3eda947f11ebffdaa002c3c2f3afd3ae3
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-05 17:29:38 +0300

    minor decorations

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,10 @@
 import { TableReservation } from './i-face-example';
 import { ApiResponse } from './i-face-response';
 
-export const api = async (form: Partial<TableReservation>): Promise<ApiResponse<TableReservation>> => {
-  return Promise.reject(new Error("not implemented yet"));
+interface Api{
+  (
+    form: Partial<TableReservation>,
+  ): Promise<ApiResponse<TableReservation>>
 }
+
+export const api:Api = async (form) => Promise.reject(new Error(`not implemented yet ${JSON.stringify(form)}`));
